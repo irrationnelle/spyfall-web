a9718996aaf069914416e763603946f16e0bff4e
irrationnelle <drakkarverenis@gmail.com>
2020-07-19 16:24:23 +0900

    chore: init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('메인화면에서는', () => {
+  describe('설정 관련 화면으로', () => {
+    let getByCurrentText: any;
+
+    beforeEach(() => {
+      const { getByText } = render(<App />);
+      getByCurrentText = getByText;
+    })
+
+    it('시간 입력이 나온다.', () => {
+      const linkElement = getByCurrentText('Time');
+      expect(linkElement).toBeInTheDocument();
+    });
+
+    it('플레이어 숫자 입력이 나온다.', () => {
+      const linkElement = getByCurrentText('Player');
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react';
+import {
+  Container, Typography, Box, Link, InputLabel, MenuItem, FormControl, Select, TextField
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const Copyright: React.FC = (): ReactElement => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Copyright © '}
+    <Link color="inherit" href="https://material-ui.com/">
+      Your Website
+    </Link>
+    {' '}
+    {new Date().getFullYear()}
+    .
+  </Typography>
+);
+
+const App:React.FC = (): ReactElement => {
+  const classes = useStyles();
+  const [time, setTime] = React.useState(5);
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTime(event.target.value as number);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="time-select-input-label">Time</InputLabel>
+          <Select
+            labelId="time-select-label"
+            id="time-select"
+            value={time}
+            onChange={handleChange}
+          >
+            <MenuItem value={5}>5 min</MenuItem>
+            <MenuItem value={10}>10 min</MenuItem>
+            <MenuItem value={15}>15 min</MenuItem>
+          </Select>
+        </FormControl>
+        <form noValidate autoComplete="off">
+          <TextField
+            id="player-input"
+            label="Player"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form>
+        <Copyright />
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
