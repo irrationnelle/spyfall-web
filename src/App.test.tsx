3977d782cb9e00eae68742c0eb0d92f9db40115c
irrationnelle <drakkarverenis@gmail.com>
2020-07-19 20:28:22 +0900

    feat: implement countPlayer and category

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('메인화면에서는', () => {
   describe('설정 관련 화면으로', () => {
     let getByCurrentText: any;
+    let getByCurrentLabelText: any;
+    let queryByCurrentLabelText: any;
+    let inputCountPlayerElement: HTMLElement;
 
     beforeEach(() => {
-      const { getByText } = render(<App />);
+      const { getByText, getByLabelText, queryByLabelText } = render(<App />);
       getByCurrentText = getByText;
-    })
+      getByCurrentLabelText = getByLabelText;
+      queryByCurrentLabelText = queryByLabelText;
+      inputCountPlayerElement = getByLabelText('Player');
+    });
 
     it('시간 입력이 나온다.', () => {
       const linkElement = getByCurrentText('Time');
@@ -19,6 +25,28 @@ describe('메인화면에서는', () => {
     it('플레이어 숫자 입력이 나온다.', () => {
       const linkElement = getByCurrentText('Player');
       expect(linkElement).toBeInTheDocument();
+    });
+
+    it('플레이어 숫자가 2명 이하이면 에러 메시지가 나온다.', () => {
+      // when
+      fireEvent.change(inputCountPlayerElement, { target: { value: 2 } });
+
+      // then
+      const errInputCountPlayerElement = queryByCurrentLabelText('Player');
+      expect(errInputCountPlayerElement).not.toBeInTheDocument();
+      const currentInputCountPlayerElement = getByCurrentLabelText('Error');
+      expect(currentInputCountPlayerElement).toBeInTheDocument();
+    });
+
+    it('플레이어 숫자가 9명 이상이면 에러 메시지가 나온다.', () => {
+      // when
+      fireEvent.change(inputCountPlayerElement, { target: { value: 9 } });
+
+      // then
+      const errInputCountPlayerElement = queryByCurrentLabelText('Player');
+      expect(errInputCountPlayerElement).not.toBeInTheDocument();
+      const currentInputCountPlayerElement = getByCurrentLabelText('Error');
+      expect(currentInputCountPlayerElement).toBeInTheDocument();
     });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import {
-  Container, Typography, Box, Link, InputLabel, MenuItem, FormControl, Select, TextField
+  Container, Typography, Box, Link, InputLabel, MenuItem, FormControl, Select, TextField,
 } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -26,12 +26,30 @@ const Copyright: React.FC = (): ReactElement => (
   </Typography>
 );
 
+const DEFAULT_COUNT_PLAYER = 4;
+const DEFAULT_TIME_MINUTE = 5;
+enum CategoryList {
+  ALL = 'all',
+  BASIC = 'basic',
+  COUNTRY = 'country',
+  STRANGE = 'strange'
+}
+
 const App:React.FC = (): ReactElement => {
   const classes = useStyles();
-  const [time, setTime] = React.useState(5);
+  const [time, setTime] = useState(DEFAULT_TIME_MINUTE);
+  const [countPlayer, setCountPlayer] = useState(DEFAULT_COUNT_PLAYER);
+  const [category, setCategory] = useState<CategoryList>(CategoryList.BASIC);
 
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  const countPlayerError = countPlayer <= 2 || countPlayer > 8;
+  const handleTime = (event: React.ChangeEvent<{ value: unknown }>) => {
     setTime(event.target.value as number);
+  };
+  const handleCountPlayer = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setCountPlayer(event.target.value as number);
+  };
+  const handleCategory = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setCategory(event.target.value as CategoryList);
   };
 
   return (
@@ -43,7 +61,7 @@ const App:React.FC = (): ReactElement => {
             labelId="time-select-label"
             id="time-select"
             value={time}
-            onChange={handleChange}
+            onChange={handleTime}
           >
             <MenuItem value={5}>5 min</MenuItem>
             <MenuItem value={10}>10 min</MenuItem>
@@ -52,14 +70,32 @@ const App:React.FC = (): ReactElement => {
         </FormControl>
         <form noValidate autoComplete="off">
           <TextField
+            error={countPlayerError}
             id="player-input"
-            label="Player"
+            label={countPlayerError ? 'Error' : 'Player'}
             type="number"
+            value={countPlayer}
             InputLabelProps={{
               shrink: true,
             }}
+            onChange={handleCountPlayer}
+            helperText={countPlayerError && '참여 인원은 최소 3명이상 최대 8명까지 입니다.'}
           />
         </form>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="category-select-input-label">Category</InputLabel>
+          <Select
+            labelId="categroy-select-label"
+            id="category-select"
+            value={category}
+            onChange={handleCategory}
+          >
+            <MenuItem value={CategoryList.ALL}>전체</MenuItem>
+            <MenuItem value={CategoryList.BASIC}>기본</MenuItem>
+            <MenuItem value={CategoryList.COUNTRY}>국가</MenuItem>
+            <MenuItem value={CategoryList.STRANGE}>희귀</MenuItem>
+          </Select>
+        </FormControl>
         <Copyright />
       </Box>
     </Container>
