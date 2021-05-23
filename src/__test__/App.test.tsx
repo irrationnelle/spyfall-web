f40b46e47834479dfbbcfe16aa27f21881027b3a
irrationnelle <drakkarverenis@gmail.com>
2021-05-23 22:43:01 +0900

    test: Refactor app test with screen object from react-testing-library

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { mocked } from 'ts-jest/utils';
 import { RecoilRoot } from 'recoil';
 
@@ -9,45 +9,33 @@ import randomIntFromInterval from '../helper/randomIntFromInterval';
 jest.mock('../helper/randomIntFromInterval');
 
 describe('스파이 게임에서는', () => {
-  let getByCurrentText: any;
-  let getByCurrentLabelText: any;
-  let getByCurrentTestId: any;
-  let queryByCurrentLabelText: any;
-  let queryByCurrentTestId: any;
-  let inputCountPlayerElement: HTMLElement;
+  const { getByText, getByLabelText, getByTestId } = screen;
+
   const FOUR_PLAYERS = 4;
 
   beforeEach(() => {
-    const {
-      getByText, getByLabelText, getByTestId, queryByLabelText, queryByTestId,
-    } = render(
+    render(
       <RecoilRoot>
         <App />
       </RecoilRoot>,
     );
-    getByCurrentText = getByText;
-    getByCurrentLabelText = getByLabelText;
-    getByCurrentTestId = getByTestId;
-    queryByCurrentLabelText = queryByLabelText;
-    queryByCurrentTestId = queryByTestId;
-    inputCountPlayerElement = getByLabelText('Player');
   });
 
   const passStageToIdentifyForCountPlayer = (countPlayer: number) => {
     for (let i = 0; i < countPlayer; i += 1) {
-      fireEvent.click(getByCurrentText(/next player/i));
+      fireEvent.click(getByText(/next player/i));
     }
-    fireEvent.click(getByCurrentText(/skip/i));
+    fireEvent.click(getByText(/skip/i));
   };
 
   const moveNextAfterFindSpy = (playerNum: number) => {
-    fireEvent.click(getByCurrentLabelText(`Player ${playerNum}`));
-    fireEvent.click(getByCurrentText(/next player/i));
+    fireEvent.click(getByLabelText(`Player ${playerNum}`));
+    fireEvent.click(getByText(/next player/i));
   };
 
   const moveNextAfterFindPlace = (placeName: string) => {
-    fireEvent.click(getByCurrentLabelText(placeName));
-    fireEvent.click(getByCurrentText(/next player/i));
+    fireEvent.click(getByLabelText(placeName));
+    fireEvent.click(getByText(/next player/i));
   };
 
   const expectDrawGame = (countPlayer: number) => {
@@ -58,44 +46,44 @@ describe('스파이 게임에서는', () => {
       moveNextAfterFindSpy(1);
     }
 
-    expect(getByCurrentText('무승부')).toBeInTheDocument();
+    expect(getByText('무승부')).toBeInTheDocument();
   };
 
   it('게임을 시작하면 스파이 플레이어 번호를 지정한다.', () => {
     // when
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
-    fireEvent.click(getByCurrentText(/next player/i));
-    fireEvent.click(getByCurrentText('확인하기'));
+    fireEvent.click(getByText(/game start!/i));
+    fireEvent.click(getByText(/next player/i));
+    fireEvent.click(getByText('확인하기'));
 
     // then
-    expect(getByCurrentText('당신은 스파이입니다.')).toBeInTheDocument();
+    expect(getByText('당신은 스파이입니다.')).toBeInTheDocument();
   });
 
   it('게임을 시작하면 기본 장소를 지정한다.', () => {
     // when
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
-    fireEvent.click(getByCurrentText('확인하기'));
+    fireEvent.click(getByText(/game start!/i));
+    fireEvent.click(getByText('확인하기'));
 
     // then
-    expect(getByCurrentText('지정한 장소는 공항입니다')).toBeInTheDocument();
+    expect(getByText('지정한 장소는 공항입니다')).toBeInTheDocument();
   });
 
   it('플레이어가 지정된 위치와 자신의 역할을 확인하고 나면 타이머를 시작한다..', () => {
     // when
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
     for (let i = 0; i < 4; i += 1) {
-      fireEvent.click(getByCurrentText(/next player/i));
+      fireEvent.click(getByText(/next player/i));
     }
 
     // then
-    expect(getByCurrentText('스파이를 찾아내도록 합니다.')).toBeInTheDocument();
+    expect(getByText('스파이를 찾아내도록 합니다.')).toBeInTheDocument();
   });
 
   it('스파이도 답을 맞추고 플레이어의 반 이상이 답을 맞추면 무승부가 된다.', () => {
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
     passStageToIdentifyForCountPlayer(FOUR_PLAYERS);
 
     moveNextAfterFindSpy(2);
@@ -103,12 +91,12 @@ describe('스파이 게임에서는', () => {
     moveNextAfterFindSpy(2);
     moveNextAfterFindSpy(1);
     // then
-    expect(getByCurrentText('무승부')).toBeInTheDocument();
+    expect(getByText('무승부')).toBeInTheDocument();
   });
 
   it('스파이는 답을 맞추고 플레이어의 반 이상이 답을 못 맞추면 스파이 승리가 된다.', () => {
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
     passStageToIdentifyForCountPlayer(FOUR_PLAYERS);
 
     moveNextAfterFindSpy(2);
@@ -117,12 +105,12 @@ describe('스파이 게임에서는', () => {
     moveNextAfterFindSpy(1);
 
     // then
-    expect(getByCurrentText('스파이 승리')).toBeInTheDocument();
+    expect(getByText('스파이 승리')).toBeInTheDocument();
   });
 
   it('스파이는 답을 못 맞추고 플레이어의 반 이상이 답을 맞추면 플레이어 승리가 된다.', () => {
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
     passStageToIdentifyForCountPlayer(FOUR_PLAYERS);
 
     moveNextAfterFindSpy(2);
@@ -130,13 +118,13 @@ describe('스파이 게임에서는', () => {
     moveNextAfterFindSpy(2);
     moveNextAfterFindSpy(1);
     // then
-    expect(getByCurrentText('플레이어 승리')).toBeInTheDocument();
+    expect(getByText('플레이어 승리')).toBeInTheDocument();
   });
 
   it('스파이도 답을 못 맞추고 플레이어의 반 이상이 답을 못 맞추면 무승부가 된다.', () => {
     // given
     mocked(randomIntFromInterval).mockReturnValue(2);
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
     passStageToIdentifyForCountPlayer(FOUR_PLAYERS);
 
     // when, then
@@ -150,9 +138,9 @@ describe('스파이 게임에서는', () => {
     // when
     const FIVE_PLAYERS = 5;
 
-    const playerInput = getByCurrentTestId('player-input');
+    const playerInput = getByTestId('player-input');
     fireEvent.change(playerInput, { target: { value: FIVE_PLAYERS } });
-    fireEvent.click(getByCurrentText(/game start!/i));
+    fireEvent.click(getByText(/game start!/i));
 
     passStageToIdentifyForCountPlayer(FIVE_PLAYERS);
 
